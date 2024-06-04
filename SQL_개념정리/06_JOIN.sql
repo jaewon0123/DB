bf9088ee91d6fdf01bf64d23f0dd86b07b6045d6
jaewon0123 <169113696+jaewon0123@users.noreply.github.com>
2024-06-04 17:24:03 +0900

    sql 최종 실습문제

--- SQL_개념정리/06_JOIN.sql
+++ SQL_개념정리/06_JOIN.sql
@@ -186,7 +186,6 @@ EMP_ID,EMP_NAME,JOB_CODE,JOB_NAME
 209	    심봉선	    J3	    부장
 */
 
-
 /***** 
 NATURAL JOIN (자연조인) 
 동일한 타입과 이름을 가진 컬럼이 있는 테이블 간의 조인을 간단히 표현한 방법
@@ -215,3 +214,73 @@ JOIN과 FROM 에서 테이블을 설정할 때
     FROM 테이블명 A,테이블명 B
     WHERE A.컬럼명 = B.컬럼명;
 *****/
+
+-- ***** OUTER JOIN 외부조인 ***** --
+-- 두 테이블의 지정하는 컬럼값이 이치하지 않는 행도
+-- 조인에 포함 시킴
+
+-- OUTER JOIN 과 비교한 INNER JOIN 쿼리문
+
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE
+/*INNER*/JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+
+-- 1) LEFT [OUTER] JOIN
+-- 합치기에 사용한 두 테이블 중에서 왼쪽 편에 작성된 테이블의
+-- 컬럼수를 기준으로 JOIN
+
+-- ANSI 표준
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE LEFT JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+--> JOIN 구문 기준으로 왼쪽에 작성된 테이블의
+---> 모든 행이 최종 결과(RESULT SET)에 포함되도록 하는 JOIN
+
+-- ORACLE 표준
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE, department
+WHERE DEPT_CODE = DEPT_ID(+);
+-- DEPT_CODE와 DEPT_ID 값이 일치하지 않아도
+-- 왼쪽에 있는 컬럼에 수에 맞게 표시를 넣어라 (NULL 값이라도 넣어라)
+
+
+-- 2) RIGHT [OUTER] JOIN
+-- 합치기에 사용한 두 테이블 중에서 오른쪽 편에 작성된 테이블의
+-- 컬럼수를 기준으로 JOIN
+
+-- ANSI 표준
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE RIGHT JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+--> JOIN 구문 기준으로 오른쪽에 작성된 테이블의
+---> 모든 행이 최종 결과(RESULT SET)에 포함되도록 하는 JOIN
+
+-- ORACLE 표준
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE, department
+WHERE DEPT_CODE(+) = DEPT_ID;
+-- DEPT_CODE와 DEPT_ID 값이 일치하지 않아도
+-- 오른쪽에 있는 컬럼에 수에 맞게 표시를 넣어라 (NULL 값이라도 넣어라)
+
+
+-- 3) FULL [OUTER] JOIN
+-- 합치기에 사용한 두 테이블이 가진 모든 행을 결과에 포함
+--> 오라클에서는 FULL OUTER JOIN 구문 사용 불가
+
+-- ANSI 표준
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE FULL JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+--> JOIN 구문 기준으로 양쪽에 작성된 테이블의
+---> 모든 행이 최종 결과(RESULT SET)에 포함되도록 하는 JOIN
+
+-- ORACLE 표준
+SELECT EMP_NAME,DEPT_TITLE
+FROM EMPLOYEE, department
+WHERE DEPT_CODE(+) = DEPT_ID(+);
+-- DEPT_CODE와 DEPT_ID 값이 일치하지 않아도
+-- 양쪽에 있는 컬럼에 수에 맞게 표시를 넣어라 (NULL 값이라도 넣어라)
+/*
+ORA-01468: a predicate may reference only one outer-joined table
+01468. 00000 -  "a predicate may reference only one outer-joined table"
+*Cause:    
+*Action:
+277행, 20열에서 오류 발생
+*/
